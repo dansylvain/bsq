cdc5a65bb99ab3f827e6f8ffd31f262686a8f324
Dan SYLVAIN <dansylvain84@gmail.com>
2024-09-23 12:19:33 +0200

    funcs reordered

--- src/read_maps.c
+++ src/read_maps.c
@@ -6,7 +6,7 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 10:23:41 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/23 12:04:11 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/23 12:18:42 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,9 +64,63 @@ int	define_index(char **tab, int *argc)
 	return (i);
 }
 
-int	get_file_size()
+int	get_file_size(char **argv, int *i)
 {
-	
+	int		fd;
+	int		j;
+	char	*str;
+
+	if (open_file(&fd, argv, i) == 0)
+		return (-1) ;
+	j = 0;
+	str = "";
+	while (str)
+	{
+		str = get_next_line(fd);
+		j++;
+		if (str)
+			printf("%s", str);
+		free(str);
+	}
+	printf("\nfile size = %i\n", j);
+	close (fd);
+	return (j);
+}
+
+int	alloc_map_tab(char ***map, int j)
+{
+	*map = NULL;
+	*map = (char **)malloc(sizeof(char *) * (j - 1));
+	if (*map == NULL)
+		return (0);
+	(*map)[j - 2] = NULL;
+	return (1);
+}
+
+int	fill_map_tab(char **argv, int *i, char ***map)
+{
+	int		fd;
+	int		j;
+	char	*str;
+
+	if (open_file(&fd, argv, i) == 0)
+		return (0) ;
+	j = 0;
+	str = "";
+	while (str)
+	{
+		str = get_next_line(fd);
+		
+		if (str && j != 0)
+		{
+			(*map)[j - 1] = ft_strdup(str);
+			printf("%s", str);
+		}
+		free(str);
+		j++;
+	}
+	close (fd);
+	return (1);
 }
 
 int	read_bsq_maps(int fd, int i, int argc, char **argv)
@@ -78,48 +132,14 @@ int	read_bsq_maps(int fd, int i, int argc, char **argv)
 
 	while (i < argc)
 	{
-		get_file_size();
-		// get file size
-		if (open_file(&fd, argv, &i) == 0)
-			continue ;
-		j = 0;
-		str = "";
-		while (str)
-		{
-			str = get_next_line(fd);
-			j++;
-			if (str)
-				printf("%s", str);
-			free(str);
-		}
-		printf("\nfile size = %i\n", j);
-		close (fd);
-
-		// malloc map tab
-		map = NULL;
-		map = (char **)malloc(sizeof(char *) * (j - 1));
-		if (map == NULL)
+		j = get_file_size(argv, &i);
+		if (j == -1)
+			continue;
+		if(alloc_map_tab(&map, j) == 0)
 			return (0);
-		map[j - 2] = NULL;
-		
-		//fill tab
-		if (open_file(&fd, argv, &i) == 0)
-			continue ;
-		j = 0;
-		str = "";
-		while (str)
-		{
-			str = get_next_line(fd);
-			
-			if (str && j != 0)
-			{
-				map[j - 1] = ft_strdup(str);
-				printf("%s", str);
-			}
-			free(str);
-			j++;
-		}
-		close (fd);
+		if(fill_map_tab(argv, &i, &map) == 0)
+			return (0);
+					
 		free_tab(map);
 		i++;
 		
