963335f1b8244351c7e578e8497e1d98d4c139fa
Dan SYLVAIN <dansylvain84@gmail.com>
2024-09-23 13:22:14 +0200

    data extraction done

--- src/read_maps.c
+++ src/read_maps.c
@@ -6,7 +6,7 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 10:23:41 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/23 12:21:20 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/23 13:21:51 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,8 @@
 #include <stdlib.h>
 
 char	*get_next_line(int fd);
-void	*ft_memset(void *s, int c, size_t n);
-char	**ft_split(char *str, char *cs);
 void	free_tab(char **tab);
 char	*ft_strdup(const char *s);
-
-char	**handle_user_input(int *argc, char ***argv)
-{
-	char	buff[100];
-	char	**tab;
-
-	tab = NULL;
-	ft_memset(buff, '\0', 100);
-	if (*argc == 1)
-	{
-		read(1, buff, 100);
-		tab = ft_split(buff, " \n	");
-		*argv = tab;
-	}
-	return (tab);
-}
 
 int	open_file(int *fd, char **argv, int *i)
 {
@@ -47,21 +29,6 @@ int	open_file(int *fd, char **argv, int *i)
 		return (0);
 	}
 	return (1);
-}
-
-int	define_index(char **tab, int *argc)
-{
-	int	i;
-
-	i = 1;
-	if (tab)
-	{
-		i = 0;
-		*argc = 0;
-		while (tab[*argc])
-			(*argc)++;
-	}
-	return (i);
 }
 
 int	get_file_size(char **argv, int *i)
