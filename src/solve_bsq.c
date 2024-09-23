92236a36a373bdfffe39c21653e5763be0281ad3
Dan SYLVAIN <dansylvain84@gmail.com>
2024-09-23 20:05:42 +0200

    created func calculate square len

--- src/solve_bsq.c
+++ src/solve_bsq.c
@@ -6,7 +6,7 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 13:23:21 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/23 19:22:13 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/23 20:05:23 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,32 @@
 #include "header.h"
 #include <unistd.h>
 
+void	calculate_square_len(t_map *map, int *i, int *j, int *max_len)
+{
+	
+}
+
 void	solve_map(t_map *map)
 {
 	int	i;
 	int	j;
-	
+	int	max_len;
+
 	printf("\n[%i], [%c], [%c], [%c]\n", map->map_size, map->empty,
 		map->obstacle, map->full);
-
+	max_len = 0;
 	i = 0;
 	while (map->map[i])
 	{
 		j = 0;
 		while (map->map[i][j])
 		{
+			calculate_square_len(map, &i, &j, &max_len);	
 			write (1, &map->map[i][j], 1);
 			j++;
 		}
 		i++;
 	}
 	printf("\n");
+	printf("bsq len: %i\n", max_len);
 }
