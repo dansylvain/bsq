88ee19a1df70e57db69eaacf2c68bdbd3172fc87
Dan SYLVAIN <dansylvain84@gmail.com>
2024-09-23 14:12:24 +0200

    extracted all data from file

--- src/read_maps.c
+++ src/read_maps.c
@@ -6,7 +6,7 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 10:23:41 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/23 13:28:41 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/23 14:09:47 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "header.h"
 
 char	*get_next_line(int fd);
 void	free_tab(char **tab);
 char	*ft_strdup(const char *s);
-void	solve_map(char **map);
+void	solve_map(t_map map);
+int		ft_atoi(const char *nptr);
+int		ft_isdigit(char str);
 
 int	open_file(int *fd, char **argv, int *i)
 {
@@ -32,7 +35,24 @@ int	open_file(int *fd, char **argv, int *i)
 	return (1);
 }
 
-int	get_file_size(char **argv, int *i)
+void	extract_map_data(char *str, t_map *map)
+{
+	int	i;
+
+	map->map_size = ft_atoi(str);
+	// printf("parse moi ca: %i\n", ft_atoi(str));
+	i = 0;
+	while (ft_isdigit(str[i]))
+		str[i++];
+	map->empty = *&str[i++];
+	map->obstacle = *&str[i++];
+	map->full = *&str[i++];
+	// printf("reste de la string: %c\n", *&str[i++]);
+	// printf("reste de la string: %c\n", *&str[i++]);
+	// printf("reste de la string: %c\n", *&str[i]);
+}
+
+int	get_file_size(char **argv, int *i, t_map *map)
 {
 	int		fd;
 	int		j;
@@ -45,6 +65,8 @@ int	get_file_size(char **argv, int *i)
 	while (str)
 	{
 		str = get_next_line(fd);
+		if(j == 0)
+			extract_map_data(str, map);
 		j++;
 		free(str);
 	}
@@ -90,20 +112,20 @@ int	read_bsq_maps(int fd, int i, int argc, char **argv)
 {
 	char	*str;
 	int		j;
-	char	**map;
+	t_map	map;
 	int		k;
 
 	while (i < argc)
 	{
-		j = get_file_size(argv, &i);
+		j = get_file_size(argv, &i, &map);
 		if (j == -1)
 			continue ;
-		if (alloc_map_tab(&map, j) == 0)
+		if (alloc_map_tab(&map.map, j) == 0)
 			return (0);
-		if (fill_map_tab(argv, &i, &map) == 0)
+		if (fill_map_tab(argv, &i, &map.map) == 0)
 			return (0);
 		solve_map(map);
-		free_tab(map);
+		free_tab(map.map);
 		i++;
 	}
 	return (1);
--- src/solve_bsq.c
+++ src/solve_bsq.c
@@ -6,20 +6,22 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 13:23:21 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/23 13:37:19 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/23 14:11:34 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "header.h"
 
-void	solve_map(char **map)
+void	solve_map(t_map map)
 {
 	int	i;
 
 	i = 0;
-	while (map[i])
+	while (map.map[i])
 	{
-		printf("%s", map[i++]);
+		printf("%s", map.map[i++]);
 	}
+		printf("\n[%i], [%c], [%c], [%c]\n", map.map_size, map.empty, map.obstacle, map.full);
 	printf("\n");
 }
