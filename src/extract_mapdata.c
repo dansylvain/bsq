19b15fc93d0bb7ee30d952e5b5cd7abf0a5b8fad
Dan SYLVAIN <dansylvain84@gmail.com>
2024-09-24 18:13:28 +0200

    ************** RESTART HERE - ALGO COMPLETE -NORMINETTE PLEASED - PARSING REMAINS *****

--- src/extract_mapdata.c
+++ src/extract_mapdata.c
@@ -6,7 +6,7 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 16:55:37 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/24 17:06:05 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:12:31 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,49 @@
 
 int	get_bsq_len(char *str);
 
-void	handle_line()
+void	handle_line(int i[], int *j, t_map **map, char *str)
 {
-	
+	if (i[0] == 0)
+	{
+		(*map)->map_size_y = ft_atoi(str);
+		*j = 0;
+		while (ft_isdigit(str[*j]))
+			(*j)++;
+		(*map)->empty = str[*j];
+		(*map)->obstacle = str[*j + 1];
+		(*map)->full = str[*j + 2];
+	}
+	else
+	{
+		if ((*map)->map_size_x == 0)
+			(*map)->map_size_x = get_bsq_len(str);
+		*j = 0;
+		while (*j < (*map)->map_size_x)
+		{
+			(*map)->map[i[1]][*j] = str[*j];
+			(*j)++;
+		}
+		i[1]++;
+	}
 }
 
 void	extract_map_data(int fd, t_map *map)
 {
 	char	*str;
-	int		i;
+	int		i[2];
 	int		j;
-	int		k;
 
 	str = "";
-	i = 0;
-	k = 0;
+	i[0] = 0;
+	i[1] = 0;
 	while (str)
 	{
 		str = get_next_line(fd);
 		if (str)
 		{
-			handle_line();
-			if (i == 0)
-			{
-				map->map_size_y = ft_atoi(str);
-				j = 0;
-				while (ft_isdigit(str[j]))
-					j++;
-				map->empty = str[j];
-				map->obstacle = str[j + 1];
-				map->full = str[j + 2];
-			}
-			else
-			{
-				if (map->map_size_x == 0)
-					map->map_size_x = get_bsq_len(str);
-				j = 0;
-				while (j < map->map_size_x)
-				{
-					map->map[k][j] = str[j];
-					j++;
-				}
-				k++;
-			}
+			handle_line(i, &j, &map, str);
 			free(str);
 		}
-		i++;
+		i[0]++;
 	}
 }
