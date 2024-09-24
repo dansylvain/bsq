d7383d0c14cd5a7663299dea6532adfb6675a7dc
Dan SYLVAIN <dansylvain84@gmail.com>
2024-09-24 11:23:30 +0200

    data extracted from file

--- inc/header.h
+++ inc/header.h
@@ -6,7 +6,7 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 13:34:35 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/24 11:02:41 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/24 11:21:14 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include "gnl.h"
 
 typedef struct map
 {
-
+	int		map_size_y;
+	int		map_size_x;
+	char	empty;
+	char	obstacle;
+	char	full;
 }	t_map;
+
+int	ft_atoi(const char *nptr);
+int	ft_isdigit(char str);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,67 @@
 /*   By: dsylvain <dsylvain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 10:04:46 by dsylvain          #+#    #+#             */
-/*   Updated: 2024/09/24 11:03:13 by dsylvain         ###   ########.fr       */
+/*   Updated: 2024/09/24 11:23:02 by dsylvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
+int	open_file(char *map_name)
+{
+	int	fd;
+	
+	fd = open(map_name, O_RDONLY);
+	return (fd);
+}
 int	main(int argc, char **argv)
 {
+	t_map map;
+	int	fd;
+	int	i;
+	int	j;
+	char *str;
+
+	// open file
+	if (argc == 2)
+		fd = open_file(argv[1]);
+			if (fd == -1)
+				return (printf("could not open file\n"), 1);
+			
+	// copy file content
+	str = "";
+	i = 0;
+	while (str)
+	{
+		str = get_next_line(fd);
+		if(str)
+		{
+			if (i == 0)
+			{
+				map.map_size_y = ft_atoi(str);
+				printf("%i", map.map_size_y);
+				j = 0;
+				while (ft_isdigit(str[j]))
+					j++;
+				map.empty = str[j];
+				map.obstacle = str[j + 1];
+				map.full = str[j + 2];
+				
+				printf("%c%c%c\n", map.empty, map.obstacle, map.full);
+			}
+			else
+				printf("%s", str);
+			free(str);
+		}
+		i++;
+	}	
+
+
+	if (fd != -1)
+	{
+		close (fd);
+		printf("file closed\n");
+	}
 	printf("Welcome to the Jungle\n");
 	return (0);
 }
